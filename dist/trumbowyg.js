60ead38026b9bfda2ade23573758cd3ebbbcc6bd
Bastian Probian <contmp@me.com>
2018-08-22 04:08:47 +0200

    (╯°□°）╯︵ ┻━┻ (╯°□°）╯︵ ┻━┻

--- dist/trumbowyg.js
+++ dist/trumbowyg.js
@@ -1281,12 +1281,12 @@ Object.defineProperty(jQuery.trumbowyg, 'defaultOptions', {
                 return url;
             }
 
-            let VALID_LINK_PREFIX = /^([a-z][-+.a-z0-9]*:|\/|#)/i;
+            var VALID_LINK_PREFIX = /^([a-z][-+.a-z0-9]*:|\/|#)/i;
             if (VALID_LINK_PREFIX.test(url)) {
                 return url;
             }
 
-            let SIMPLE_EMAIL_REGEX = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+            var SIMPLE_EMAIL_REGEX = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
             if (SIMPLE_EMAIL_REGEX.test(url)) {
                 return 'mailto:' + url;
             }
